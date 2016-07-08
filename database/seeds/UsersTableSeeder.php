e564f3707fe313712b98a6fe80f8666e9cef441a
Victor Souto <souto.victor@gmail.com>
2016-07-08 16:29:59 -0300

    fix

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -19,27 +19,6 @@ class UsersTableSeeder extends Seeder
         $user1 = \App\User::create([
             'name'  => 'kz',
             'email' => 'souto.victor@gmail.com',
-            'password' => \Illuminate\Support\Facades\Hash::make('123'),
+            'password' => \Illuminate\Support\Facades\Hash::make('cracker0'),
         ]);
-        $user2 = \App\User::create([
-            'name'  => $faker->name,
-            'email' => $faker->email,
-            'password' => \Illuminate\Support\Facades\Hash::make('123'),
-        ]);
-        $user3 = \App\User::create([
-            'name'  => $faker->name,
-            'email' => $faker->email,
-            'password' => \Illuminate\Support\Facades\Hash::make('123'),
-        ]);
-        $user4 = \App\User::create([
-            'name'  => $faker->name,
-            'email' => $faker->email,
-            'password' => \Illuminate\Support\Facades\Hash::make('123'),
-        ]);
-        $user5 = \App\User::create([
-            'name'  => $faker->name,
-            'email' => $faker->email,
-            'password' => \Illuminate\Support\Facades\Hash::make('123'),
-        ]);
-    }
 }
