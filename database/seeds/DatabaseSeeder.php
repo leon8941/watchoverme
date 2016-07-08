feae86c368f476aec123136662f35de0e56284b0
Victor Souto <souto.victor@gmail.com>
2016-07-08 17:27:35 -0300

    fix

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
+        $lithium = \App\User::create([
+            'name' => 'Lithium',
+            'password' => \Illuminate\Support\Facades\Hash::make('12345'),
+            'email' => 'lithiumbloodthirst@gmail.com'
+        ]);
+        
+        //$this->call(UsersTableSeeder::class);
         //$this->call(PostsTableSeeder::class);
         //$this->call(GamersTableSeeder::class);
         //$this->call(EventsTableSeeder::class);
