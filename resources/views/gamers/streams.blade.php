47417ce53d84c535e9101d87ac04ab7b0082137e
Victor Souto <souto.victor@gmail.com>
2017-06-09 19:30:32 -0300

    fix

--- resources/views/gamers/streams.blade.php
+++ resources/views/gamers/streams.blade.php
@@ -17,7 +17,7 @@
         @foreach($streams as $stream)
             <div class="image isotope-item">
                 <div class="image-inner">
-                    <a href="{{ route('users.show', [$stream->slug]) }}" data-lightbox="humor" >
+                    <a href="http://twitch.tv/{{ $stream->twitch }}" target="_blank" data-lightbox="humor" >
                         <img src="{{ $stream->twitch_logo }}" alt="" />
                     </a>
                     <p class="image-caption">
