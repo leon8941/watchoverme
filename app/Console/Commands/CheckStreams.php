adf344dd9dd165401bcdec7a2f505a96c3040152
Victor Souto <souto.victor@gmail.com>
2017-06-09 19:03:33 -0300

    fix

--- app/Console/Commands/CheckStreams.php
+++ app/Console/Commands/CheckStreams.php
@@ -67,6 +67,7 @@ class CheckStreams extends Command
             $i++;*/
             $info = $this->getTwitchChannelInfo($user->twitch);
 
+            print_r($info);
             $user->update([
                 'twitch_status' => $info['mature'],
                 'twitch_followers' => $info['followers'],
